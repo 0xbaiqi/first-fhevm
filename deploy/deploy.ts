03d38263e652b4c582e7a9eec9854c268a986538
baiqi <495199985@qq.com>
2025-07-07 17:12:23 +0800

    开发测试合约

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,9 +1,11 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+//0xA5e526b0e1259A5CD0E816B1dB06251B5D6a0EA0
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
+  console.log("deployer", deployer);
 
   const deployedFHECounter = await deploy("FHECounter", {
     from: deployer,
